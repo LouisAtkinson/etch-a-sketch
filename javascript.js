d479c056c2861ca24a6b182e1e868c56f0e6cda7
Louis <119425566+nimzoid@users.noreply.github.com>
2023-01-04 15:46:19 +0000

    Create button for user to change number of cells

--- javascript.js
+++ javascript.js
@@ -1,13 +1,49 @@
 const grid = document.querySelector('#grid');
+const button = document.getElementById("btn");
 
-for (let i = 1; i < 16*16; i++) {
+button.addEventListener("click", createGrid);
+
+for (let i = 0; i < 16*16; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.style.height = "50px";
+    cell.style.width = "50px";
+    grid.style.gridTemplateColumns = "repeat(16, 50px)";
+    grid.style.gridTemplateRows = "repeat(16, 50px)";
     grid.appendChild(cell);
     cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = "red";
     })    
 }
+
+function reset() {
+    grid.innerHTML = '';
+  }
+
+function createGrid() {
+    let input = prompt("How many squares per side? (Maximum 100)", "16");
+    let size = parseInt(input);
+    if (size > 0 && size < 101) {
+        reset();
+        for (let i = 0; i < size*size; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.height = 800 / size + "px";
+            cell.style.width = 800 / size + "px";
+            grid.style.gridTemplateColumns = "repeat(" + size + ", " + 800 / size + "px)";
+            grid.style.gridTemplateRows = "repeat(" + size + ", " + 800 / size + "px)";
+            grid.appendChild(cell);
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = "red";
+            })    
+        }
+    } else {
+        alert("Please enter a number between 1 and 100");
+    }
+
+}
+
+
 
 
 

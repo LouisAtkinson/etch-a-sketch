24f39471696edc276ee92d86ba796ec42dd45fbc
Louis Atkinson <119425566+LouisAtkinson@users.noreply.github.com>
2023-01-10 22:58:28 +0000

    Add slider and clear button and color scheme

--- javascript.js
+++ javascript.js
@@ -1,50 +1,50 @@
 const grid = document.querySelector('#grid');
-const button = document.getElementById("btn");
+let cells = grid.children;
+grid.classList.add('grid');
 
-button.addEventListener("click", createGrid);
+let slider = document.getElementById("myRange");
+let gridSize = document.querySelector(".output");
+gridSize.innerHTML = slider.value + " x " + slider.value;
 
-for (let i = 0; i < 16*16; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.style.height = "50px";
-    cell.style.width = "50px";
-    grid.style.gridTemplateColumns = "repeat(16, 50px)";
-    grid.style.gridTemplateRows = "repeat(16, 50px)";
-    grid.appendChild(cell);
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = "red";
-    })    
+slider.oninput = function() {
+    gridSize.innerHTML = slider.value + " x " + slider.value;
 }
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', createGrid)
+
+function createGrid() {
+    reset();
+    for (let i = 0; i < slider.value*slider.value; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.height = 700 / slider.value + "px";
+        cell.style.width = 700 / slider.value + "px";
+        grid.style.gridTemplateColumns = "repeat(" + slider.value + ", " + 700 / slider.value + "px)";
+        grid.style.gridTemplateRows = "repeat(" + slider.value + ", " + 700 / slider.value + "px)";
+        grid.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "#957dad";
+        })    
+    }
+}
+
+slider.addEventListener("change", function () {
+    reset();
+    for (let i = 0; i < slider.value*slider.value; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.height = 700 / slider.value + "px";
+        cell.style.width = 700 / slider.value + "px";
+        grid.style.gridTemplateColumns = "repeat(" + slider.value + ", " + 700 / slider.value + "px)";
+        grid.style.gridTemplateRows = "repeat(" + slider.value + ", " + 700 / slider.value + "px)";
+        grid.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "#957dad";
+        })    
+    }
+})
 
 function reset() {
     grid.innerHTML = '';
-  }
-
-function createGrid() {
-    let input = prompt("How many squares per side? (Maximum 100)", "16");
-    let size = parseInt(input);
-    if (size > 0 && size < 101) {
-        reset();
-        for (let i = 0; i < size*size; i++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.style.height = 800 / size + "px";
-            cell.style.width = 800 / size + "px";
-            grid.style.gridTemplateColumns = "repeat(" + size + ", " + 800 / size + "px)";
-            grid.style.gridTemplateRows = "repeat(" + size + ", " + 800 / size + "px)";
-            grid.appendChild(cell);
-            cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = "red";
-            })    
-        }
-    } else {
-        alert("Please enter a number between 1 and 100");
-    }
-
 }
-
-
-
-
-
-
